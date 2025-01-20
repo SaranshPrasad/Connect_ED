0834cb71fed39f2df8c0fdf6828f6eb59b7700c6
SaranshPrasad <saranshprasad08@gmail.com>
2025-01-20 16:14:49 +0530

    Some minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Home from './Components/Home';
-import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Login from './Components/Login';
 import UserProfile from './Components/UserProfile';
 import Feed from './Components/Feed';
 import UserPosts from './Components/UserPosts';
 import AddPostForm from './Components/AddPostForm';
-import ProfilePage from './Components/ProfilePage';
-import ProfileCards from './Components/ProfileCards';
+
+
 import Chat from './Components/Chat';
 import Connections from './Components/Connections';
 import { Provider } from 'react-redux';
@@ -26,8 +26,6 @@ const App = () => {
       <Route path='/user/feed' element={<Feed/>}></Route>
       <Route path='user/post/:userId' element={ <UserPosts/>}></Route>
       <Route path='/user/post' element={ <AddPostForm/>}></Route>
-      <Route path='/profilepage' element={ <ProfilePage/>}></Route>
-      <Route path='/profilecard' element={ <ProfileCards/>}></Route>
       <Route path='/chat/:toUserName/:toUserId' element={ <Chat/>}></Route>
       <Route path='/connections' element={ <Connections/>}></Route>
       <Route path='/connections/requests' element={ <ConnectionRequests/>}></Route>
